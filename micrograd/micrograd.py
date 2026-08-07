161f978973c985d5eb0365c77cc075a68815bc9c
nossyd <dysson415@gmail.com>
2026-08-07 15:23:55 -0700

    update micrograd

--- micrograd/micrograd.py
+++ micrograd/micrograd.py
@@ -1,13 +1,14 @@
 class Value:
-    def __init__(self, data, _children=()):
+    def __init__(self, data, _children=(), op=''):
         self.data = data
         self._prev = set(_children)
+        self.op = op
 
     def __repr__(self):
         return(f"Value = {self.data}")
 
     def __add__(self, other):
-        return Value(self.data + other.data, (self, other))
+        return Value(self.data + other.data, (self, other), '+')
     
     def __mul__(self, other):
-        return Value(self.data * other.data, (self, other))
+        return Value(self.data * other.data, (self, other), '*')
